19a4171805d49dff54d5e4829af5e991faf6f2bd
Vishruth <vishruthdevan@gmail.com>
2021-12-14 21:06:58 +0530

    OCR using pytesseract

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import pytesseract
 import discord
 import os
 from dotenv import load_dotenv
@@ -18,9 +20,12 @@ async def on_message(message):
         return
 
     if message.content.startswith('$ocr'):
-        await message.channel.send('Processing image...')
+        await message.channel.send('>>> Processing image...')
         for i in message.attachments:
             await i.save(i.filename)
+            text = pytesseract.image_to_string(Image.open(i.filename))
+            os.remove(i.filename)
+            await message.channel.send('```' + text + '```')
 
 
 client.run(os.getenv('TOKEN'))
